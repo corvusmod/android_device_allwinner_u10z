4501180be50407bb9648799eb712d8c36c257532
Fredrik Karlsson <fkrlsn@gmail.com>
2013-02-09 02:02:27 +0100

    novo9: update path
    Change-Id: I2f1a46c2d9fcdc00b9db60d661765e8540c3f4b9

--- full_novo9.mk
+++ full_novo9.mk
@@ -1,3 +1,3 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
-$(call inherit-product, device/allwinner/novo9/novo9.mk)
+$(call inherit-product, device/allwinner/novo9/device_novo9.mk)
